f059740b91759df07a9f0e4a1bdd1be253ae5280
Yumi Yukimura <me.cafebabe@gmail.com>
2026-04-13 02:10:13 +0800

    mainline/generic: Stop abusing APEX for selecting among non-APEX HALs
    It doesn't even work anymore on latest Android version ;\
    
    Change-Id: I319419c3d5a7420b34f0de650afe2991df5f1c64

--- services/generic_init/mount_helpers.cpp
+++ services/generic_init/mount_helpers.cpp
@@ -18,11 +18,38 @@
 #include "switch_root.h"
 
 #include <android-base/logging.h>
+#include <android-base/strings.h>
 #include <fs_mgr.h>
 #include <fs_mgr_overlayfs.h>
 #include <fstab/fstab.h>
+#include <sys/stat.h>
+
+#include <filesystem>
 
 namespace MountHelpers {
+
+namespace fs = std::filesystem;
+
+static void CreateOverlayfsUpperdirAndWorkdirIfNotPresent(const FstabEntry& entry) {
+    for (const auto& fs_option : android::base::Split(entry.fs_options, ",")) {
+        auto equal_sign = fs_option.find('=');
+        if (equal_sign == std::string::npos) continue;
+
+        const auto param = fs_option.substr(0, equal_sign);
+        const auto arg = fs_option.substr(equal_sign + 1);
+
+        if (param != "upperdir" && param != "workdir") continue;
+        if (TryAccessDir(arg)) continue;
+
+        LOG(INFO) << "Creating " << param << " for overlayfs mount point "
+                  << entry.mount_point << ": " << arg;
+
+        // TODO: Bring mkdir_recursive() to util.cpp and use it
+        if (mkdir(arg.c_str(), 0755)) {
+            LOG(ERROR) << "Failed to create directory " << arg;
+        }
+    }
+}
 
 static bool GetRootEntry(FstabEntry* root_entry) {
     Fstab proc_mounts;
@@ -45,6 +72,18 @@ static bool GetRootEntry(FstabEntry* root_entry) {
     *root_entry = std::move(*entry);
 
     return true;
+}
+
+bool TryAccessDir(const std::string& path) {
+    std::error_code ec;
+    fs::file_status s = fs::status(path, ec);
+    return (!ec && fs::exists(s) && fs::is_directory(s));
+}
+
+bool TryAccessFile(const std::string& path) {
+    std::error_code ec;
+    fs::file_status s = fs::status(path, ec);
+    return (!ec && fs::exists(s) && fs::is_regular_file(s));
 }
 
 bool MountPartition(Fstab& fstab_, const Fstab::iterator& begin,
@@ -123,6 +162,7 @@ bool MountPartitions(Fstab& fstab_) {
 
     for (const auto& entry : fstab_) {
         if (entry.fs_type == "overlay") {
+            CreateOverlayfsUpperdirAndWorkdirIfNotPresent(entry);
             fs_mgr_mount_overlayfs_fstab_entry(entry);
         }
     }
