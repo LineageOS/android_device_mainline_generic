b15c72b564bdb063b894eaf50f59416ecea7b485
Yumi Yukimura <me.cafebabe@gmail.com>
2026-04-16 02:01:10 +0800

    [WIP] mainline/generic: Filter out unwanted sections on debian kconfig, and use gki_defconfig
    Change-Id: Iefec57d3f33e5dc0d638da1e7e2a37288bbed2ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,9 +71,12 @@ $(call soong_config_set,minigbm_upstream,platform,all)
 $(call soong_config_set_bool,drmfb_composer,uses_minigbm,true)
 
 # Kernel
+TARGET_KERNEL_CONFIG := gki_defconfig
 TARGET_KERNEL_CONFIG_EXT := \
-    $(DEVICE_PATH)/configs/kernel/debian.config \
+    $(DEVICE_PATH)/configs/kernel/pre-debian.config \
+    $(PRODUCT_OUT)/obj/KCONFIG_OBJ/debian-filtered.config \
     $(DEVICE_PATH)/configs/kernel/fix-build.config \
+    kernel/mainline/configs/fragments/y/fbcon.config \
     $(DEVICE_PATH)/configs/kernel/customizations.config
 TARGET_KERNEL_SOURCE ?= kernel/mainline/android-mainline
 
