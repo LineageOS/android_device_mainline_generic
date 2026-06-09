69dbbc7d9950130a937a40d593d7810640ca3fe7
Yumi Yukimura <me.cafebabe@gmail.com>
2026-06-09 21:27:30 +0800

    mainline/generic: Add kernel build configuration for Asahi Linux kernel
    Change-Id: If4ec52190f06adfbb4f2163dab3400784536a7b3

---
+++ build/kernels/asahi.mk
@@ -0,0 +1,18 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_KERNEL_SOURCE ?= kernel/apple/asahi
+
+TARGET_KERNEL_CONFIG := \
+    defconfig \
+    asahi.config
+
+TARGET_KERNEL_CONFIG_EXT := \
+    kernel/mainline/configs/defconfigs/arm64/gki_defconfig \
+    kernel/apple/asahi/arch/arm64/configs/asahi.config \
+    kernel/mainline/configs/fragments/y/arm64/pagesize-16k.config \
+    kernel/mainline/configs/fragments/y/fbcon.config \
+    kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
+    kernel/mainline/configs/fragments/n/faster-build-time.config
