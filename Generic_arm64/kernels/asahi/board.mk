e508bcd159490835be7b1bffc9a98142c2566aed
Yumi Yukimura <me.cafebabe@gmail.com>
2026-06-09 21:27:30 +0800

    mainline/generic: Add kernel build configuration for Asahi Linux kernel
    Change-Id: If4ec52190f06adfbb4f2163dab3400784536a7b3

---
+++ Generic_arm64/kernels/asahi/board.mk
@@ -0,0 +1,30 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_SOURCE := kernel/apple/asahi
+
+ifeq ($(KERNEL_ASAHI_USE_GKI_DEFCONFIG),true)
+# Does not work for now
+TARGET_KERNEL_CONFIG_EXT := \
+    kernel/mainline/configs/defconfigs/arm64/gki_defconfig \
+    $(TARGET_DEVICE_PATH)/kernels/asahi/fixup-deps.config \
+    $(TARGET_KERNEL_SOURCE)/arch/arm64/configs/asahi.config \
+    kernel/mainline/configs/fragments/y/fbcon.config \
+    kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
+    kernel/mainline/configs/fragments/n/faster-build-time.config \
+    $(DEVICE_PATH)/configs/kernel/customizations.config
+else
+TARGET_KERNEL_CONFIG := \
+    defconfig \
+    asahi.config
+TARGET_KERNEL_CONFIG_EXT :=
+endif
+
+TARGET_KERNEL_CONFIG_EXT += \
+    kernel/mainline/configs/fragments/y/fbcon.config \
+    kernel/mainline/configs/fragments/n/disable-clang-hardening-features.config \
+    kernel/mainline/configs/fragments/n/faster-build-time.config \
+    $(DEVICE_PATH)/configs/kernel/customizations.config
