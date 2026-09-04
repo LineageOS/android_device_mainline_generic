f94a42398261bd30d5565bf022c7997889ccd2b5
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-05 02:13:47 +0800

    mainline/generic: Drop the now obsolete HAL service selection logic
    Change-Id: I85b7244706bf26e2f9d42fb5a22a9443fc5d80ac

--- device.mk
+++ device.mk
@@ -103,7 +103,6 @@ PRODUCT_PACKAGES += \
     device_generic_logging.rc \
     device_generic_settings.rc \
     device_generic_settings.sh \
-    hal_services.rc \
     init-chowns.sh \
     init_dev_config_override.rc
 
